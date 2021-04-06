24d79a43a3c4b89d27e1256e3760e9343d8edb69
nguyenchung2405 <chungdaik@gmail.com>
2021-04-06 21:34:39 +0700

    React Routing

---
+++ src/redux/configStore.js
@@ -0,0 +1,8 @@
+import { combineReducers, createStore } from 'redux';
+
+const rootReducer = combineReducers({
+    // Chứa các state của ứng dụng
+
+})
+
+export const store = createStore(rootReducer);
